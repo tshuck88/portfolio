7acdc2443ab4bf22177b503dd868065d07a5c7d2
Tyler Shuck <tshuck88@gmail.com>
2021-01-21 22:12:12 -0800

    finished AboutMe component. Add some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavigationMenu from "./components/Navigation/NavigationMenu/NavigationMenu";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
+import AboutMe from "./components/AboutMe/AboutMe"
 
 function App() {
   return (
     <div className="App">
       <NavigationMenu />
       <Jumbotron />
+      <AboutMe />
+
     </div>
   );
 }
