130ebf73727f88e22293097c85c577f3e325ce5f
Tyler Shuck <tshuck88@gmail.com>
2021-01-25 15:03:13 -0800

    changed rows for md screens on about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => (
     <div className={classes.AboutMe}>
         <div className="container-fluid">
             <div className="row">
-                <div className="col-lg-4">
+                <div className="col-md-4">
                     <div className={classes.imgWrapper}>
                         <img src={bioPic} alt="Tyler Shuck"></img>
                     </div>
