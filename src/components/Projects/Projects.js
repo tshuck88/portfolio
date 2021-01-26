017baea642c5c5592f4685ef9cf6e124619c87ac
Tyler Shuck <tshuck88@gmail.com>
2021-01-25 16:55:09 -0800

    created layout for projects section. added some projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,40 +5,100 @@ const Projects = () => (
     <div className={classes.Projects}>
         <div className="container-fluid">
             <h1>My Projects</h1>
-            <div class="row row-cols-1 row-cols-md-4 g-4">
-                <div class="col">
-                    <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 g-4">
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Burger Builder</h5>
+                            <p className="card-text">Application allowing users to build and order themselves a burger. Built with React, Redux, and Firebase.</p>
+                        </div>
+                        <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                            <a href="https://github.com/tshuck88/burger-builder" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                            <a href="https://cranky-yalow-0d2e3c.netlify.app/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a short card.</p>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">VolunTeam!</h5>
+                            <p className="card-text">Volunteering and community service event website allowing users to search for and create events. Developed in a group of 4 using React, Node.js, Express, MongoDB, and more.</p>
+                        </div>
+                        <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                            <a href="https://github.com/ajperkins92/Project-3" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                            <a href="https://quiet-hamlet-85082.herokuapp.com/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Shopazon</h5>
+                            <p className="card-text">E-commerce website where users can browse and purchase products. Built in a group of 4 using JavaScript, jQuery, Node.js, Express Handlebars, MySQL, and more.</p>
+                        </div>
+                        <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                            <a href="https://github.com/jazznerd206/Shopazon" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                            <a href="https://evening-mesa-27220.herokuapp.com/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Plant-Forge</h5>
+                            <p className="card-text">Website developed for a startup company using HTML, CSS, JavaScript, MailChimp, and BigCommerce online store.</p>
+                        </div>
+                        <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                            <a href="https://github.com/tshuck88/plant-forge" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                            <a href="http://plant-forge.com/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Burger Builder</h5>
+                            <p className="card-text">Application allowing users to build and order themselves a burger. Built with React, Redux, and Firebase.</p>
+                            </div>
+                            <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                                <a href="https://github.com/tshuck88/burger-builder" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                                <a href="https://cranky-yalow-0d2e3c.netlify.app/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Burger Builder</h5>
+                            <p className="card-text">Application allowing users to build and order themselves a burger. Built with React, Redux, and Firebase.</p>
+                            </div>
+                            <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                                <a href="https://github.com/tshuck88/burger-builder" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                                <a href="https://cranky-yalow-0d2e3c.netlify.app/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Burger Builder</h5>
+                            <p className="card-text">Application allowing users to build and order themselves a burger. Built with React, Redux, and Firebase.</p>
+                            </div>
+                            <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                                <a href="https://github.com/tshuck88/burger-builder" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                                <a href="https://cranky-yalow-0d2e3c.netlify.app/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Burger Builder</h5>
+                            <p className="card-text">Application allowing users to build and order themselves a burger. Built with React, Redux, and Firebase.</p>
+                            </div>
+                            <div className={`${classes.buttonWrapper} buttonWrapper d-flex justify-content-between`}>
+                                <a href="https://github.com/tshuck88/burger-builder" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Github</button></a>
+                                <a href="https://cranky-yalow-0d2e3c.netlify.app/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-outline-dark">Demo</button></a>
                         </div>
                     </div>
                 </div>
